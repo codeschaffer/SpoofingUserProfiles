140818e34cd022e03b6e16f58866fe350d6c2d0b
codyschaffer <codeschaffer@gmail.com>
2015-02-24 16:21:30 -0700

    r

---
+++ SpoofingUserProfiles/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SpoofingUserProfiles.Startup))]
+namespace SpoofingUserProfiles
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
